dee80182af133c6a3667aadd67a96422477fa14b
Mackral02 <mackral02@gmail.com>
2022-04-29 22:16:04 +0530

    minor changes

--- update-twitter-status5.php
+++ update-twitter-status5.php
@@ -4,7 +4,7 @@
 
 	$twitterCharacterLimit = 270;
 	$twitterDatasetPath = 'twitter/dataset/';
-	$twitterCurrentDataset = 'json1.json';
+	$twitterCurrentDataset = 'json2.json';
 	$imageBackgroundURL = 'https://picsum.photos/800/800.jpg';
 	$responseJSON = [];
 
